d6377feba5be4f7b346ff7b6873061ff628e5852
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-10-21 14:58:13 -0500

    Updated to new NetworkLinux

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Flower.git", from: "0.1.0"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", from: "0.1.0"),
+        .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
