82c9a27136e1e00cad65d3b637618c8e4715e8dd
CryptoSax <joshua@operatorfoundation.org>
2021-11-08 14:48:43 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,14 +11,14 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
         .package(url: "https://github.com/OperatorFoundation/Monolith.git", from: "1.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Keychain.git", from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/ReplicantSwift.git", from: "0.13.0"),
-        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.2.1"),
+        .package(url: "https://github.com/OperatorFoundation/ReplicantSwift.git", from: "0.13.5"),
+        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.2.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.1"),
         .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.1"),
         .package(url: "https://github.com/OperatorFoundation/TransmissionTransport.git", from: "0.3.0"),
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.8"),
-        .package(url: "git@github.com:apple/swift-crypto.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
     ],
     targets: [
         .target(
