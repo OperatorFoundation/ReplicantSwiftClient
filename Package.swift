6d63b7253b852841540e4c02d26ae537fde96256
consuelita <adelita@operatorfoundation.org>
2021-11-16 17:15:39 -0600

    Use TransmissionTransport 1.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.2"),
         .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.4"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionTransport.git", from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionTransport.git", from: "1.1.0"),
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.9"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
     ],
