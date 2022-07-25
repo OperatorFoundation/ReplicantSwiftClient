fae169108fea22822c0b586c5d3718247ae39e2c
consuelita <adelita@operatorfoundation.org>
2022-07-25 16:05:48 -0500

    macOS v12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ReplicantSwiftClient",
-    platforms: [.macOS(.v10_15),
+    platforms: [.macOS(.v12),
                         .iOS(.v15)
                        ],
     products: [.library(name: "ReplicantSwiftClient", targets: ["ReplicantSwiftClient"])],
